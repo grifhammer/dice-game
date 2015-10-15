51d0b7017877e618cd48860ce433bb66d121cf3e
Griffin Hammer <grifhammer@gmail.com>
2015-10-15 12:04:37 -0400

    Initial Commit
    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function throwDie(){
+	var randomDie1 = Math.round(Math.random() * 5) + 1; //Give us a random number between 1 and 6
+	var randomDie2 = Math.round(Math.random() * 5) + 1; //Give us a random number between 1 and 6
+	document.images["myDie1"].src = "d" + randomDie1 +".gif"
+	document.images["myDie2"].src = "d" + randomDie2 +".gif"	
+	console.log(randomDie1)
+}
