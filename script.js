2b153dd8fe296809c4f777b90b46b5d192188a68
Griffin Hammer <grifhammer@gmail.com>
2015-10-15 12:08:46 -0400

    Split the throw dice functions apart
    Allows you to call function to throw one or two dice.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function throwDie(){
+function throwDice(){
 	var randomDie1 = Math.round(Math.random() * 5) + 1; //Give us a random number between 1 and 6
 	var randomDie2 = Math.round(Math.random() * 5) + 1; //Give us a random number between 1 and 6
 	document.images["myDie1"].src = "d" + randomDie1 +".gif"
 	document.images["myDie2"].src = "d" + randomDie2 +".gif"	
 	console.log(randomDie1)
+}
+function throwDiee(){
+	var randomDie = Math.round(Math.random() * 5) + 1; //Give us a random number between 1 and 6
+	document.images["myDie1"].src = "d" + randomDie +".gif"
+	console.log(randomDie)
 }
